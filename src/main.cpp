bff5d006aa27428bafff9ff813e53260139ee017
MissHertz <286863@stud.inn.no>
2025-09-22 11:15:05 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,11 @@
 #include <raylib.h>
+// Global Variables
+float windowWidth = 1800;
+float windowHeight = 950;
+float windowHalfWidth = windowWidth / 2;
+float windowHalfHeight = windowHeight / 2;
+
+
 int main()
 {
 

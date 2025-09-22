2b8832a422bf7f1ef5d2b39e6f359b62b68054c2
MissHertz <286863@stud.inn.no>
2025-09-22 11:14:50 +0200

    Update Player.h

--- src/Player/Player.h
+++ src/Player/Player.h
@@ -6,11 +6,6 @@
 //
 //};
 
-// Global Variables
-float windowWidth = 1800;
-float windowHeight = 950;
-float windowHalfWidth = windowWidth / 2;
-float windowHalfHeight = windowHeight / 2;
 
 // Global Player Variables
 float playerXPosition = windowHalfWidth;
